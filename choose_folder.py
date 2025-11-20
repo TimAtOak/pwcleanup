187127dbe28e8c35fce80728060ffe60e2e653ad
timineichen <tim.ineichen@edu.teko.ch>
2025-11-20 23:34:09 +0100

    dynamic paths

--- choose_folder.py
+++ choose_folder.py
@@ -1,27 +1,30 @@
 from InquirerPy import prompt
 from log_folders import log_folders_and_files
-
+import os
 
 def choose_folders():
+    home = os.path.expanduser("~")  # dynamic home path
+    paths = {
+        "Downloads": os.path.join(home, "Downloads"),
+        # "Desktop": os.path.join(home, "Desktop"),
+        "AppData": os.path.join(home, "AppData")
+    }
+
     sub_questions = [
         {
             "type": "list",
             "name": "subAction",
             "message": "Choose an action:",
-            "choices": ["Downloads", "Desktop", "Appdata","Back","Exit"]
+            "choices": ["Downloads", "AppData", "Back", "Exit"]
         }
     ]
 
     sub_answer = prompt(sub_questions)
-    sub_action = sub_answer["subAction"]  # Extract string
+    sub_action = sub_answer["subAction"]
 
-    if sub_action == "Downloads":
-        log_folders_and_files('C:\\Users\\ineic\\Downloads')
-    elif sub_action == "Desktop":
-        log_folders_and_files('C:\\Users\\ineic\\Desktop')
-    elif sub_action == "Appdata":
-        log_folders_and_files('C:\\Users\\ineic\\AppData')
-    elif sub_action == "Exit":
+    if sub_action in paths:
+        log_folders_and_files(paths[sub_action])
+    elif sub_action == "Back":
         from main import main_menu
         main_menu()
     elif sub_action == "Exit":
